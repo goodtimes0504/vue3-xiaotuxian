caf23be6ca6dfd4bb70fab6832c738d2bdb74708
goodtimes0504 <revolution_lc@outlook.com>
2024-11-30 19:45:37 +0800

    前台分类组件封装

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 import { getHomeBannerAPI } from '@/services/home'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
@@ -17,11 +18,20 @@ onLoad(() => {
 </script>
 
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel />
+  <!-- 商品列表 -->
   <view class="index">index</view>
 </template>
 
 <style lang="scss">
-//
+//小程序背景颜色设置
+page {
+  // background-color:#eceaea
+  background-color: #f3f1f1;
+}
 </style>
